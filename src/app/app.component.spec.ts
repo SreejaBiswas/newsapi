f153cc3af25e0cc62e764b2994744faff896ea53
SreejaBiswas <bsreeja2002@gmail.com>
2024-03-30 10:02:09 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'news-api'`, () => {
+  it(`should have as title 'news-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('news-api');
+    expect(app.title).toEqual('news-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, news-api');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('news-app app is running!');
   });
 });
